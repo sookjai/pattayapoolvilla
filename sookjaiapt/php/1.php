9e05cbfc93631742e5378c3b39f3f899b5f7f811
sookjai <34384680+sookjai@users.noreply.github.com>
2017-12-14 07:33:35 +0700

    Add files via upload

---
+++ sookjaiapt/php/1.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Sook Jai APT</title>
+  </head>
+  <body>
+    <h1>PHP</h1>
+    <?php
+        echo 10+10;
+     ?>
+     <h2>JAVA SCRIPT</h2>
+     <script>
+       document.write(10+10);
+     </script>
+  </body>
+</html>
